f7e9387e49cf20de388ba6239f5ab5ada50f67ae
Ivo Slanina <ivo.slanina@gmail.com>
2015-06-12 15:34:51 +0200

    Added first dhcp header file.

---
+++ dhcp.h
@@ -0,0 +1,53 @@
+#ifndef __DHCP_H__
+#define __DHCP_H__
+
+enum dhcp_op {
+    DHCP_OP_BOOTREQUEST         = 1,
+    DHCP_OP_BOOTREPLY           = 2
+};
+
+enum dhcp_htype {
+    DHCP_HTYPE_ETHERNET_10      = 1,
+    DHCP_HTYPE_ETHERNET_3       = 2,
+    DHCP_HTYPE_AX_25            = 3,
+    DHCP_HTYPE_PRONET           = 4,
+    DHCP_HTYPE_CHAOS            = 5,
+    DHCP_HTYPE_IEEE_802         = 6,
+    DHCP_HTYPE_ARCNET           = 7,
+    DHCP_HTYPE_HYPERCHANNEL     = 8,
+    DHCP_HTYPE_LANSTAR          = 9,
+    DHCP_HTYPE_AUTONET          = 10,
+    DHCP_HTYPE_LOCAL_TALK       = 11,
+    DHCP_HTYPE_LOCAL_NET        = 12,
+    DHCP_HTYPE_ULTRA_LINK       = 13,
+    DHCP_HTYPE_SMDS             = 14,
+    DHCP_HTYPE_FRAME_RELAY      = 15,
+    DHCP_HTYPE_ATM_16           = 16,
+    DHCP_HTYPE_HDLC             = 17,
+    DHCP_HTYPE_FIBRE_CHANNEL    = 18,
+    DHCP_HTYPE_ATM_19           = 19,
+    DHCP_HTYPE_SERIAL_LINE      = 20,
+    DHCP_HTYPE_ATM_21           = 21
+};
+
+struct dhcp_message {
+    uint8_t op;
+    uint8_t htype;
+    uint8_t hlen;
+    uint8_t hops;
+    uint32_t xid;
+    uint16_t secs;
+    uint16_t flags;
+    uint32_t ciaddr;
+    uint32_t yiaddr;
+    uint32_t siaddr;
+    uint32_t giaddr;
+    uint8_t chaddr[16];
+#ifndef UIP_CONF_DHCP_LIGHT
+    uint8_t sname[64];
+    uint8_t file[128];
+#endif
+    uint8_t options[312];
+};
+
+#endif
