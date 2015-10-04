14cb9dbf6d979fddad91bc3ff66fa82fd7123a10
Ivo Slanina <ivo.slanina@gmail.com>
2015-10-04 12:46:37 +0200

    fixed data types.

--- uart.c
+++ uart.c
@@ -25,24 +25,24 @@ void uart_init(uint16_t baudrate) {
 
     uint16_t baud_setting = F_CPU / 8 / baudrate - 1;
 
-    UBRR0H = (unsigned char) (baud_setting >> 8);
-    UBRR0L = (unsigned char) baud_setting;
+    UBRR0H = (uint8_t) (baud_setting >> 8);
+    UBRR0L = (uint8_t) baud_setting;
 
     UCSR0C = _BV(UCSZ01) | _BV(UCSZ00) | _BV(USBS0); /* 8-bit data */
     UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
 }
 
-void uart_putc(unsigned char data) {
+void uart_putc(uint8_t data) {
     UDR0 = data;
     loop_until_bit_is_set(UCSR0A, UDRE0);
 }
 
-void uart_puts(const char *s) {
+void uart_puts(char *s) {
     while (*s)
         uart_putc(*s++);
 }
 
-void uart_println(const char *s) {
+void uart_println(char *s) {
     uart_puts(s);
     uart_puts("\r\n");
 }
--- uart.h
+++ uart.h
@@ -20,9 +20,9 @@
 
 void uart_init(uint16_t baudrate);
 
-void uart_putc(unsigned char data);
+void uart_putc(uint8_t data);
 
-void uart_puts(const char *s );
+void uart_puts(char *s );
 
-void uart_println(const char *s );
+void uart_println(char *s );
 #endif // __UART_H__
